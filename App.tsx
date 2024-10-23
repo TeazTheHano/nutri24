09aa5dbdf18509d4a6dc4bf6bb9e00863213296a
Khuong Anh Kiet <teaz.khuonganhkiet@gmail.com>
2024-10-23 17:52:39 +0700

    23/10

--- App.tsx
+++ App.tsx
@@ -1,118 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import { ProviderTotal } from './data/store';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+// screen import
+import Onboard from './screens/Onboard';
+import DataCollect from './screens/DataCollect';
+import clrStyle from './assets/componentStyleSheet';
+import { ColorValue } from 'react-native';
 
+import BottomTab from './assets/BottomTab';
+import Home from './screens/Home';
+import Podcast from './screens/Podcast';
+import Explore from './screens/Explore';
+import User from './screens/User';
+// >>>>>>>>>>
+import BlogView from './screens/BlogView';
+
+// ____________________END OF IMPORT_______________________
+
+const Stack = createNativeStackNavigator();
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    <ProviderTotal>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false, contentStyle: { backgroundColor: clrStyle.white as ColorValue } }}>
+          {/* <Stack.Screen name="BottomTab" component={BottomTab} /> */}
+          <Stack.Screen name="BottomTab" component={BottomTab} />
+          <Stack.Screen name="Onboard" component={Onboard} />
+          <Stack.Screen name="DataCollect" component={DataCollect} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Podcast" component={Podcast} />
+          <Stack.Screen name="Explore" component={Explore} />
+          <Stack.Screen name="User" component={User} />
+          {/* >>>> */}
+          <Stack.Screen name="BlogView" component={BlogView} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ProviderTotal>
+  )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
+
+export const VERCOLOR = 1;
